a5852c43218716e5c86d0f3b1fc13acc7657f572
Yash Vijay <yashvijay12@hotmail.com>
2022-08-27 12:18:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function down() {
+  let Para =document.getElementById('write');
+  Para.innerHTML="ThankYou for downloading IOS application";
+}
+function down2() {
+  let Para =document.getElementById('write');
+  Para.innerHTML="ThankYou for downloading Android application";
+}
+
+function sentmail() {
+  let Para =document.getElementById('con');
+  Para.innerHTML="Mail sent successfully";
+}
